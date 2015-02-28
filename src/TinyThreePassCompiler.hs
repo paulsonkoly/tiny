30b6dd2222a61f877774604eaf3fa956968435b4
Paul Sonkoly <sonkoly.pal@gmail.com>
2015-02-28 13:22:53 +0000

    First pass working.

--- src/TinyThreePassCompiler.hs
+++ src/TinyThreePassCompiler.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
 module TinyThreePassCompiler where
+
+import qualified Data.Map as M
+import Control.Monad.State
+import Data.Maybe (listToMaybe)
+import Control.Lens
+import Control.Applicative
+import Data.Maybe (fromJust)
+
 
 data AST = Imm Int
          | Arg Int
@@ -28,15 +37,96 @@ tokenize xxs@(c:cs)
     (i, is) = span (`elem` digit) xxs
     (s, ss) = span (`elem` alpha) xxs
 
-compile :: String -> [String]
-compile = pass3 . pass2 . pass1
+
+------------------------------------------------------------------------------
+-- | Parser.
+data ParserSate = ParserSate { _input     :: [ Token ]
+                             , _pos       :: Int
+                             , _variables :: M.Map String Int
+                             }
+makeLenses ''ParserSate
+type Parser a = (StateT ParserSate Maybe) a
+
+
+runParser :: Parser a -> [Token] -> Maybe a
+runParser p i = evalStateT p (ParserSate i 0 M.empty)
+
+
+next :: Parser Token
+next = do
+  t <- liftM listToMaybe $ use input
+  input %= tail
+  lift t
+
+
+token :: Token -> Parser ()
+token (TChar x) = next >>= \t -> guard $ t == TChar x
+token _         = undefined
+
+
+variable :: Parser ()
+variable = next >>= \t -> case t of
+  (TStr name) -> do
+    p <- pos <%= succ
+    variables %= M.insert name (p - 1)
+  _           -> mzero
+
+
+value :: Parser AST
+value = immVal <|> varVal
+  where
+    immVal = next >>= \t -> case t of
+      (TInt i) -> return $ Imm i
+      _        -> mzero
+    varVal = next >>= \t -> case t of
+      (TStr name) -> do
+        p <- liftM (M.lookup name) $ use variables
+        lift $ Arg <$> p
+      _           -> mzero
+
+
+operator :: Parser AST -> Token -> Parser AST -> Parser AST
+operator l (TChar o) r = do
+  l' <- l
+  token (TChar o)
+  r' <- r
+  return $ toAST o l' r'
+operator _ _ _ = undefined
+
+
+toAST :: Char -> (AST -> AST -> AST)
+toAST '+' = Add
+toAST '-' = Sub
+toAST '*' = Mul
+toAST '/' = Div
+toAST _   = undefined
 
 
 pass1 :: String -> AST
-pass1 = undefined
+pass1 = fromJust . runParser function . tokenize
+  where
+    function      = do
+      token (TChar '[') *> argument_list <* token (TChar ']')
+      expression
+    argument_list = void $ many variable
+    expression    = operator term (TChar '+') expression
+                    <|> operator term (TChar '-') expression
+                    <|> term
+    term          = operator factor (TChar '*') term
+                    <|> operator factor (TChar '/') term
+                    <|> factor
+    factor        = token (TChar '(') *> expression <* token (TChar ')')
+                    <|> value
+
 
 pass2 :: AST -> AST
 pass2 = undefined
 
+
 pass3 :: AST -> [String]
 pass3 = undefined
+
+
+compile :: String -> [String]
+compile = pass3 . pass2 . pass1
+
